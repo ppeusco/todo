89acdfa934ad6622829c212db4953aecd2f153cc
ppeusco <pablo.peuscovich@globant.com>
2014-05-03 20:33:13 -0300

    Editing Finished

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,32 @@
 class TasksController < ApplicationController
   def new   
   	@task = Task.new 
+    render :show_form
   end
 
   def create     
   	@task = Task.create(task_params)     
-  	redirect_to root_path   
+  	@tasks = Task.all
+    render :hide_form
   end   
+
+  def destroy 
+    @task = Task.find(params[:id]) 
+    @task.destroy 
+    @tasks = Task.all 
+  end 
+
+  def edit 
+    @task = Task.find(params[:id]) 
+    render :show_form
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update_attributes(task_params)
+    @tasks = Task.all
+    render :hide_form
+  end
 
   private   
   def task_params     
